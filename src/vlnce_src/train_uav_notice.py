db0f7529f44732f6bb0c8450230d09be057ca555
yutong_z22 <15606221+yutongz22@user.noreply.gitee.com>
2025-09-18 20:43:22 +0800

    get_input_embeddings, get_output_embeddings, requires_grad_

--- src/vlnce_src/train_uav_notice.py
+++ src/vlnce_src/train_uav_notice.py
@@ -568,7 +568,8 @@ def smarter_tokenizer_and_embedding_resize(
             dim=0, keepdim=True)
         output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(
             dim=0, keepdim=True)
-
+        model.get_input_embeddings().weight.requires_grad_(True)
+        model.get_output_embeddings().weight.requires_grad_(True)
         input_embeddings[-num_new_tokens:] = input_embeddings_avg
         output_embeddings[-num_new_tokens:] = output_embeddings_avg
 
